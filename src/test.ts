b3b2320df7422c78cfad02719c9b7f0d7c129f7c
igloo15 <mtolland15@gmail.com>
2020-05-31 22:52:45 -0400

    #Change reformat document back to old format

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,20 @@
-import './style.scss';
-import { ActionEngine } from './action-engine';
+import "./style.scss";
+import { ActionEngine } from "./action-engine";
 
 console.log("Hello World");
 
 const actionEngine = new ActionEngine(23, 1000);
 
 actionEngine.addAnimationAction((context, elapsed, frame) => {
-    console.log(`Normal Action - Context: ${context}, Elapsed: ${elapsed}, Frame: ${frame}`);
+  console.log(
+    `Normal Action - Context: ${context}, Elapsed: ${elapsed}, Frame: ${frame}`
+  );
 }, 2000);
 
 actionEngine.addPreciseAction((context, elapsed, frame) => {
-    console.log(`Precision Action - Context: ${context}, Elapsed: ${elapsed}, Frame: ${frame}`);
+  console.log(
+    `Precision Action - Context: ${context}, Elapsed: ${elapsed}, Frame: ${frame}`
+  );
 }, 3000);
 
 actionEngine.start();
